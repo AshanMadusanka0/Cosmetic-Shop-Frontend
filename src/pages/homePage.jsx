84f822ffbb46baa702e1bb451dd41952461e397e
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-07-19 08:16:07 +0530

    add the telwind css

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,7 +4,7 @@ import './homePage.css';
 export default function HomePage() {
   return (
     <div className="home-container">
-      <h1>Welcome to the Home Page</h1>
+      <h1 className='text-red-600'>Welcome to the Home Page</h1>
       <p>This is a simple homepage styled with CSS.</p>
       <button>Explore More</button>
       
