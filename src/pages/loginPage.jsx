b4d3b52188dce9632a9f594b08a750164ad8290f
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-07-24 11:12:14 +0530

    update

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
+
+
+  const [email,setEmail]=useState("Your Email")   /////
+  const [password,setPassword]=useState("")      /////
+
+
   return (
     <div className='w-full h-screen bg-amber-600 flex justify-center items-center'>
       <div className='w-[450px] h-[450px] bg-green-700 flex flex-col justify-center items-center'>
         <img src='/logo.png' className='w-[120px] '/>
        
             <span>Email</span>
-            <input type="text" placeholder="Enter E-mail" className='bg-amber-50'/>
+            <input type="text" className='bg-amber-50' defaultValue={email} onChange={
+              (e)=>{
+                console.log(e.target.value)         ////
+              }
+            }/>    
 
             <span>Password</span>
-            <input type="password" placeholder="Enter password" className='bg-amber-50'/>
+            <input type="password" className='bg-amber-50' defaultValue={password}/>
             <button>Login</button>
           
 
