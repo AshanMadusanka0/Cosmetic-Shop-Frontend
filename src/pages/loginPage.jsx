da313fd0f0d29ab59869850f35e496eb203c0374
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-07-24 10:42:12 +0530

    login page re design

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -3,7 +3,16 @@ import { Link } from 'react-router-dom';
 export default function LoginPage() {
   return (
     <div className='w-full h-screen bg-amber-600 flex justify-center items-center'>
-      <div className='w-[450px] h-[450px] bg-blue-800'>
+      <div className='w-[450px] h-[450px] bg-green-700 flex flex-col justify-center items-center'>
+        <img src='/logo.png' className='w-[120px] '/>
+       
+            <span>Email</span>
+            <input type="text" placeholder="Enter E-mail" className='bg-amber-50'/>
+
+            <span>Password</span>
+            <input type="password" placeholder="Enter password" className='bg-amber-50'/>
+            <button>Login</button>
+          
 
       </div>
 
