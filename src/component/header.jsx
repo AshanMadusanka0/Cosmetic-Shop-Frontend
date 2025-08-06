8e57af57434879410fc98d13d8ece7f1c7f8aae4
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-08-06 22:48:36 +0530

    navbar color changed

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
    return(
-    <header className="bg-gray-100 w-full h-[75px] relative flex justify-center items-center">
+    <header className="bg-blue-400 w-full h-[75px] relative flex justify-center items-center">
         <img src="/logo.png" className="w-[100px] rounded-full absolute left-[10px] cursor-pointer"/>
         <div className="h-full flex items-center w-[600px] justify-evenly">
         <Link to="/" className="text-orange-700 font-semibold text-xl hover:border-b border-b-amber-700">Home</Link>
